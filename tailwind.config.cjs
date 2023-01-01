e7d3f7b8476d1ad9666581b0bdec4b3a3b5162c4
DarkDev <mda.business.on@gmail.com>
2023-01-01 17:55:59 +0500

    Setup TailwindCSS and Public Files

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,22 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
-	},
-	plugins: [],
+		extend: {
+		  fontFamily: {
+			"dm-sans": "'DM Sans', sans-serif",
+			"inter": "'Inter', sans-serif",
+			"poppins": "'Poppins', sans-serif"
+		  }
+		},
+		keyframes:{
+		  "fading":{
+			"0%" : {opacity:"0"},
+			"100%": {opacity:"100"}
+		  }
+		},
+		animation:{
+		  "fade": "fading 3s linear 1"
+		}
+	  },
+	  plugins: [],
 }
